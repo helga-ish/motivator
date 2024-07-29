2c7559d4eba14ecd1432489406a51c11aaa0012f
Helga Ish <belfegor.nomnom@gmail.com>
2024-07-29 23:29:02 +0800

    first version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { notes } from './notes';
+import flower1 from './images/flower1.svg';
 
 function App() {
+
+// делаем стейт для языков
+  const [language, setLanguage] = React.useState('en');
+
+  // достаем рандомную запись из словарика
+  const [note, setNote] = React.useState("");
+  function getRandomNote() {
+    const randomIndex = Math.floor(Math.random() * notes.length);
+    return setNote(notes[randomIndex]);
+  }
+
+  const getNoteText = (note) => {
+    if (note) {
+      return note.translations[language] || note.translations['en']
+    }
+    return '';
+  }
+
+
+  // делаем стейты и функции для смены кнопки на текст
+  const [isOpen, setIsOpen] = React.useState(false);
+
+    const [isVisible, setIsVisible] = React.useState(true);
+    function handleVisibleState() {
+        setIsVisible(true);
+        setIsOpen(false);
+    }
+    function handleNotVisibleState() {
+      setIsVisible(false);
+      setIsOpen(true);
+      getRandomNote();
+    }
+
+  // переключаем классы в зависимости от того, что открыто: кнопка или текст
+  const noteOpen = `motivation-note-container ${isOpen ? '' : 'motivation-note-container_disabled'}`;
+  const buttonVisible = `button ${!isVisible ? 'button_disabled' : ''}`;
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="change-languages">
+          <button className='change-languages_button' onClick={() => setLanguage('en')}>English</button>
+          <button className='change-languages_button' onClick={() => setLanguage('ru')}>Русский</button>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <button className={buttonVisible} onClick={!isVisible ? handleVisibleState : handleNotVisibleState}>Find your motivation and affirmation here</button>
+        <div className={noteOpen}>
+          <p className="motivation-note">{getNoteText(note)}</p>
+          <div className='motivation-note-back-container'>
+            <img src={flower1} className='note-flower' onClick={!isVisible ? handleVisibleState : handleNotVisibleState} />
+            <button className='note-button' onClick={!isVisible ? handleVisibleState : handleNotVisibleState}>Back</button>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/notes.js
@@ -0,0 +1,360 @@
+export const notes = [
+    {
+      id: 1,
+      translations: {
+        en: "You are doing great today. \nAnd your hair looks awesome!",
+        ru: "Ты отлично справляешься. \nА какая шикарная у тебя прическа сегодня!"
+      }
+    },
+    {
+      id: 2,
+      translations: {
+        en: "Your skin is glowing today! \nSlay your day!",
+        ru: "У тебя кожа буквально светится! \nСегодня точно твой день!"
+      }
+    },
+    {
+      id: 3,
+      translations: {
+        en: "You are enough and your efforts are too. \nJust enjoy the day!",
+        ru: "Ты делаешь и стараешься достаточно. \nПросто насладись сегодняшним днем!"
+      }
+    },
+    {
+      id: 4,
+      translations: {
+        en: "The best version of you is you already. \nHave a blast today!",
+        ru: "Ты уже лучшая версия себя. \nСегодня будет потрясный день!"
+      }
+    },
+    {
+      id: 5,
+      translations: {
+        en: "Just because it's hard doesn't mean it's impossible, you can do everything.",
+        ru: "Просто потому что тебе сложно что-то сделать, \nне значит, что это невозможно. \nТы можешь все."
+      }
+    },
+    {
+      id: 6,
+      translations: {
+        en: "A little progress each day adds up to big results. \nBut if you wanna rest, just be a coach potato today.",
+        ru: "Даже маленький прогресс ведет к большому результату. \nНо если ты хочешь передохнуть сегодня, забей на все и отдохни."
+      }
+    },
+    {
+      id: 7,
+      translations: {
+        en: "You are doing really well, keep going, hang in there. \nYou got this!",
+        ru: "Ты отлично справляешься, продолжай в том же духе. \nУ тебя все получится!"
+      }
+    },
+    {
+      id: 8,
+      translations: {
+        en: "You are so good at anything, \ndon't be hard on yourself, okay?",
+        ru: "У тебя получается все, за что ты берешься, так что поменьше строгости к себе, хорошо?"
+      }
+    },
+    {
+      id: 9,
+      translations: {
+        en: "It's okay if you are overthinking it. \nBut if it bothers you, try to be more present, \nbreathe deeper and slower. \nIt's gonna be fine!",
+        ru: "Ничего страшного, если ты накручиваешь себя. \nНо если тебя это напрягает, то постарайся быть в моменте, \nдыши медленее и глубже. \nВсе будет хорошо!"
+      }
+    },
+    {
+      id: 10,
+      translations: {
+        en: "It's okay if you feel insecure today. \nWe both know you slay, yeah?",
+        ru: "Ничего страшного, если ты чувствуешь уязвимость сегодня. \nМы же знаем, что ты просто отпад!"
+      }
+    },
+    {
+      id: 11,
+      translations: {
+        en: "It's okay if you struggle to stay positive. \nBut remember that every cloud has a silver lining.",
+        ru: "Ничего страшного, если тебе не удается быть на позитиве. \nНо не забывай, нет худа без добра."
+      }
+    },
+    {
+      id: 12,
+      translations: {
+        en: "It's okay if you made a mistake or don't know what to do next. \nA samurai has no goal, only path. \nYour way is the best way for you.",
+        ru: "Ничего страшного, если ты совершаешь ошибку или не знаешь, что делать дальше. \nУ самурая нет цели, только путь. \nИ твой путь идеально подходит для тебя."
+      }
+    },
+    {
+      id: 13,
+      translations: {
+        en: "Spoiler alert: you totally got this.",
+        ru: "Спойлер: у тебя все точно получится."
+      }
+    },
+    {
+      id: 14,
+      translations: {
+        en: "If you don't want to do something, try at least for five minutes. \nBut there is nothing wrong with you if you rest today.",
+        ru: "Если оченьт не хочется что-то делать, то начни хотя бы с пяти минут. \nНо ничего страшного, если ты просто отдохнешь сегодня."
+      }
+    },
+    {
+      id: 15,
+      translations: {
+        en: "Don't push yourself too hard and take care of your health. \nLove ya!",
+        ru: "Не дави так на себя и позаботься о своем здоровье. \nЛюблю тебя!"
+      }
+    },
+    {
+      id: 16,
+      translations: {
+        en: "Breathe and remember who the heck you are. \nWhatever you do, you slay!",
+        ru: "Выдохни и вспомни, кто ты. \nУ тебя что угодно получится на 100%!"
+      }
+    },
+    {
+      id: 17,
+      translations: {
+        en: "Your value is not based on your productivity. \nEnjoy the moment!",
+        ru: "Твоя ценность как человека не связана с твоей продуктивностью. \nНаслаждайся моментом!"
+      }
+    },
+    {
+      id: 18,
+      translations: {
+        en: "There is no right time, there is just time & what you choose to do with it. \nYou always can choose your own comfort and spend time as you wish.",
+        ru: "Не существует идеального момента, есть просто момент и ты решаешь, как его использовать. \nИ ты всегда можешь выбрать собственный комфорт \nи потратить время на то, чего хочешь именно ты."
+      }
+    },
+    {
+      id: 19,
+      translations: {
+        en: "You deserve all the good things, you are powerful and strong. \nHave a good day!",
+        ru: "Ты заслуживаешь всего самого лучшего, у тебя достаточно сил и способностей. \nХорошего тебе дня!"
+      }
+    },
+    {
+      id: 20,
+      translations: {
+        en: "Oh my, you look so good today. \nDon't doubt yourself!",
+        ru: "Божечки, как здорово ты выглядишь. \nНе сомневайся в себе!"
+      }
+    },
+    {
+      id: 21,
+      translations: {
+        en: "You are one of a kind. \nThank you for being yourself!",
+        ru: "Таких, как ты больше нет. \nСпасибо за то, что остаешься собой!"
+      }
+    },
+    {
+      id: 22,
+      translations: {
+        en: "A beautiful day begins with a beautiful mindset. \nYou deserve all the blessings and miracles in the world. Have them!",
+        ru: "Прекрасный день начинается с прекрасного настроя. \nТы заслуживаешь всего наилучшего и всех возможных чудес. \nПускай они все у тебя случатся!"
+      }
+    },
+    {
+      id: 23,
+      translations: {
+        en: "The love you attract is better than you could have possibly imagined. \nYou are awesome.",
+        ru: "Любовь, что ты притягиваешь гораздо прекраснее, чем ты можешь себе представить. \nТы невероятный человек."
+      }
+    },
+    {
+      id: 24,
+      translations: {
+        en: "Your life is not a to-do list. \nDon't forget to really enjoy it.",
+        ru: "Твоя жизнь - не просто список дел. \nНе забудь по-настоящему наслаждаться ей."
+      }
+    },
+    {
+      id: 25,
+      translations: {
+        en: "Your boundaries are important. \nDon't let anybody ruin your mood today! You got this!",
+        ru: "Твои границы важны. \nНе позволяй кому-либо испортить тебе настроение сегодня! У тебя все получится!"
+      }
+    },
+    {
+      id: 26,
+      translations: {
+        en: "Your feelings are valid. \nListen to your heart more, dear!",
+        ru: "Твои чувства важны. \nПрислушивайся чаще к своему сердцу, чудесный человек!"
+      }
+    },
+    {
+      id: 27,
+      translations: {
+        en: "Do your thing and don't care if they like it.",
+        ru: "Делай свое дело и не переживай, что другие подумают."
+      }
+    },
+    {
+      id: 28,
+      translations: {
+        en: "Remember, there is always someone out there and you are a rainbow in their cloud.",
+        ru: "Запомни, где-то есть человек, для которого ты - лучик солнца в темном царстве."
+      }
+    },
+    {
+      id: 29,
+      translations: {
+        en: "A dead end is just a good place to turn around. \nIt's not a waste of time, \njust think everything through and make new decisions.",
+        ru: "Тупик - отличное место, чтобы совершить поворот на 180 градусов. \nЭто значит, что пришло время все обдумать и прийти к новому решению."
+      }
+    },
+    {
+      id: 30,
+      translations: {
+        en: "If you are being puzzled today in any way, \njust say yes and you’ll figure it out afterwards.",
+        ru: "Если тебя сегодня озадачили каким-то предложением, \nпросто прими его, а разберешься потом."
+      }
+    },
+    {
+      id: 31,
+      translations: {
+        en: "You can’t make a cloudy day a sunny day, \nbut you can embrace it and decide \nit’s going to be a good day after all.",
+        ru: "Ты не можешь превратить пасмурный день в солнечный, \nзато можешь принять его и решить для себя, \nчто он все равно будет прекрасным."
+      }
+    },
+    {
+      id: 32,
+      translations: {
+        en: "In your life expect some trouble. \nBut when you worry, you make it double. \nBut don't worry, be happy, be happy now.",
+        ru: "В жизни будут происходить всякие неприятности. \nНо они становятся в два раза хуже, если из-за них переживать. \nНе волнуйся, будеть счастлив, причем прямо сейчас."
+      }
+    },
+    {
+      id: 33,
+      translations: {
+        en: "Keep your face toward the sunshine today, \nand shadows will fall behind you.",
+        ru: "Сегодня повернись лицом к солнечному свету \nи пускай тени этого дня останутся позади."
+      }
+    },
+    {
+      id: 34,
+      translations: {
+        en: "Do anything, but let it produce joy today.",
+        ru: "Делай что угодно сегодня, \nно пускай это принесет тебе радость."
+      }
+    },
+    {
+      id: 35,
+      translations: {
+        en: "You've got nothing to do today but smile.",
+        ru: "У тебя нет больше никаких дел на сегодня, \nкроме как улыбаться."
+      }
+    },
+    {
+      id: 36,
+      translations: {
+        en: "Anything is possible with sunshine in your soul and a little pink.",
+        ru: "Все возможно, если в твоей душе светит солнце, а на тебе надето что-нибудь розовое."
+      }
+    },
+    {
+      id: 37,
+      translations: {
+        en: "You carry the passport to your own calmness today. \nUse it and take a rest.",
+        ru: "Твое спокойствие сегодня - в твоих руках. \nВоспользуйся им и передохни."
+      }
+    },
+    {
+      id: 38,
+      translations: {
+        en: "You don't need somebody else's magic to make your day. \nYou are the one magical creature yourself.",
+        ru: "Тебе не нужна чья-то магия, чтобы сделать твой день. \nТы сам по себе то еще волшебное существо."
+      }
+    },
+    {
+      id: 39,
+      translations: {
+        en: "Just believe in yourself. \nEven if you don’t, pretend that you do and, \nat some point, you will.",
+        ru: "Поверь в себя. \nА даже если не получится, притворись, что веришь, \nи когда-нибудь это станет правдой."
+      }
+    },
+    {
+      id: 40,
+      translations: {
+        en: "If there is something hard ahead of you today, \nbelieve you can and you're halfway there.",
+        ru: "Если тебя сегодня ждет какое-то сложное дело, \nпросто поверь, что справишься, а это уже полдела."
+      }
+    },
+    {
+      id: 41,
+      translations: {
+        en: "If you see someone without a smile, give 'em yours today. \nYour smile is gorgeous!",
+        ru: "Если увидишь кого-то хмурого сегодня, улыбнись этому человеку. \nУ тебя чудесная улыбка!"
+      }
+    },
+    {
+      id: 42,
+      translations: {
+        en: "You are imperfect, you are wired for struggle, \nbut you are worthy of love and belonging.",
+        ru: "Ты не идеален, ты создан, чтобы сражаться, \nно вместе с тем ты достоин любви и быть частью чего-то."
+      }
+    },
+    {
+      id: 43,
+      translations: {
+        en: "Act today as if what you do makes a difference. \nIt does.",
+        ru: "Сегодня действуй так, как будто все, что ты делаешь, имеет значение. \nПотому что так и есть."
+      }
+    },
+    {
+      id: 44,
+      translations: {
+        en: "You are your best thing.",
+        ru: "Ты - лучшее, что у тебя есть."
+      }
+    },
+    {
+      id: 45,
+      translations: {
+        en: "If you're stressed out, you must have something like a pie today. \nStress cannot exist in the presence of a pie.",
+        ru: "Если ты сегодня стрессуешь, скушай кусок пирога или что-нибудь такое. \nНевозможно стрессовать в присутствии пирога."
+      }
+    },
+    {
+      id: 46,
+      translations: {
+        en: "No need to hurry. No need to sparkle. \nNo need to be anybody but oneself today.",
+        ru: "Нет нужды торопиться. Нет нужны сиять. \nСегодня тебе не нужно быть никем другим, только собой."
+      }
+    },
+    {
+      id: 47,
+      translations: {
+        en: "If you are down, remember: \nalmost everything will work again \nif you unplug it for a few minutes, including you.",
+        ru: "Если ты в раздрае, помни практически универсальный метод: \nвыдерни из розетки и включи снова. \nЭто работает и с людьми."
+      }
+    },
+    {
+      id: 48,
+      translations: {
+        en: "If you are afraid of something, don't forget: \nThe things we fear the most have already happened to us.",
+        ru: "Если ты боишься чего-то, не забывай: \nто, чего мы страшимся больше всего, \nэто то, что уже случалось с нами."
+      }
+    },
+    {
+      id: 49,
+      translations: {
+        en: "You’ll never find a rainbow if you’re looking down. \nBut some days you can see its reflection in the puddles. \nRelax and choose your own way today.",
+        ru: "Ты не увидишь радугу, если будешь идти с опущенной головой. \nНо иногда ее можно заметить в лужах после дождя. \nРасслабься и выбери сам, что тебе подходит сегодня."
+      }
+    },
+    {
+      id: 50,
+      translations: {
+        en: "You are allowed to feel messed up and inside out. \nIt doesn't mean you're defective — it just means you're human.",
+        ru: "Ты можешь быть в раздрае и чувствовать себя не в своей тарелке. \nЭто не значит, что ты какой-то неполноценный, это значит, что ты - человек."
+      }
+    },
+    {
+      id: 51,
+      translations: {
+        en: "Life is 10 percent what you experience and 90 percent how you respond to it. \nStaying positive may make your day a bit better!",
+        ru: "Жизнь на 10% состоит из твоего опыта и на 90% из того, как ты на него реагируешь. \nОтносясь ко всему немного позитивнее, ты можешь сделать свой день чуточку лучше!"
+      }
+    }
+  ];
+  
